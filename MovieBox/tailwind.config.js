ebe1309a62dc9344d51ee9d8201bee13e23955fd
wheval <princegbefavor@gmail.com>
2023-09-11 03:32:34 +0100

    configure custom theme

--- MovieBox/tailwind.config.js
+++ MovieBox/tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{html,js}",
+    ".index.html",
+],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: '#f7fafc',
+          400: '#9CA3AF',
+          500: '#6B7280',
+          900: '#111827',
+        },
+        rose : {
+          700: '#BE123C',
+        },
+        red: {
+          600: '#9CA3AF',
+        },
+        lightGray: '#F3F4F680'
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
